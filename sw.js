a15ece02e677aa24a03760c8522f0bbbddd81fab
nagapurinarayana-ship-it <nagapuri.narayana@gmail.com>
2026-08-17 02:13:44 +0530

    Bust cache after entertainment filtering fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'movieblog-v13-live-content-white';
+const CACHE_NAME = 'movieblog-v14-live-content-white';
 const BASE = new URL('./', self.registration.scope).pathname;
 const OFFLINE_URL = `${BASE}offline.html`;
 const IMAGE_BASE = 'https://image.tmdb.org/t/p/w500';
